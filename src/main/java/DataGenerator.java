8c35046596bff725e7aca5e417905685bfeff2bc
Sergey Boyko <sergejuswfb@gmail.com>
2023-07-10 22:08:30 +0300

    correct

---
+++ src/main/java/DataGenerator.java
@@ -0,0 +1,49 @@
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    Faker faker = new Faker(new Locale("ru"));
+
+    public String generateDate(int daysToAdd) {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate deliveryDate = currentDate.plusDays(daysToAdd);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String deliveryDateStr = deliveryDate.format(formatter);
+        return deliveryDateStr;
+    }
+
+    public String generateName() {
+        return faker.name().name();
+    }
+
+    public String generatePhoneNumber() {
+        return faker.numerify("+###########");
+    }
+
+    public String generateCity() {
+        return faker.address().city();
+    }
+
+    public static class Registration {
+        private static UserInfo user;
+
+        private Registration() {
+        }
+        public static UserInfo generateUser(String locale) {
+            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
+            // generateName(locale), generatePhone(locale)
+            return user;
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
